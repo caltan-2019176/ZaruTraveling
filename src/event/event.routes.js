e2defc874453cb3dc552e5279e0ef84f6aed59ea
rdiaz-2022178 <rdiaz-2022178@kinal.edu.gt>
2024-04-08 18:02:04 -0600

    se crea el CRUD completo de Event

--- src/event/event.routes.js
+++ src/event/event.routes.js
@@ -1,6 +1,12 @@
 import {Router} from 'express'
+import { add, deleted, find, test, update } from './event.controller.js'
 
 const api = Router()
 
+api.get('/test', test)
+api.post('/add', add)
+api.put('/update/:id', update)
+api.get('/find', find)
+api.delete('/delete/:id', deleted)
 
 export default api
