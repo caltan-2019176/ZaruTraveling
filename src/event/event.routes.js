52deae2e85609eb08e22431565b55048637c7cd5
rdiaz-2022178 <rdiaz-2022178@kinal.edu.gt>
2024-04-08 18:50:17 -0600

    se agregan metodos de seguridad para ciertas acciones

--- src/event/event.routes.js
+++ src/event/event.routes.js
@@ -1,12 +1,14 @@
-import {Router} from 'express'
+import { Router } from 'express'
+import { validateJwt, isAdmin } from '../middlewares/validate-jwt.js'
+
 import { add, deleted, find, test, update } from './event.controller.js'
 
 const api = Router()
 
 api.get('/test', test)
-api.post('/add', add)
-api.put('/update/:id', update)
+api.post('/add', [validateJwt, isAdmin], add)
+api.put('/update/:id', [validateJwt, isAdmin], update)
 api.get('/find', find)
-api.delete('/delete/:id', deleted)
+api.delete('/delete/:id', [validateJwt, isAdmin], deleted)
 
 export default api
